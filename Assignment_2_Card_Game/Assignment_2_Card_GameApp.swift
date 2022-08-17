829a828cfafa1c15949e46cf76769749f9c8f90d
Triet_Luu <s3594528@rmit.edu.vn>
2022-08-17 09:05:29 +0700

    Initial Commit

---
+++ Assignment_2_Card_Game/Assignment_2_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_2_Card_GameApp.swift
+//  Assignment_2_Card_Game
+//
+//  Created by Triet, Luu Huynh on 17/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_2_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment_2_Card_Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment_2_Card_Game
+//
+//  Created by Triet, Luu Huynh on 17/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
