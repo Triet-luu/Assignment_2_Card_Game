7ec0c7038e075cb4f6bea44dcb11b3259d71fe14
Triet-luu <56990982+Triet-luu@users.noreply.github.com>
2022-08-29 22:56:26 +0700

    Update OldMaidControl.swift

--- Assignment_2_Card_Game/View/OldMaidControl.swift
+++ Assignment_2_Card_Game/View/OldMaidControl.swift
@@ -1,9 +1,16 @@
-//
-//  OldMaidEngine.swift
-//  Assignment_2_Card_Game
-//
-//  Created by Triet, Luu Huynh on 28/08/2022.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 2
+  Author: Luu Huynh Triet
+  ID: s3594528
+  Created  date: 17/08/2022
+  Last modified: 29/08/2022
+  Acknowledgement: https://bicyclecards.com/how-to-play/old-maid/
+ https://github.com/lcs-jliu/OldMaid/blob/master/OldMaid/main.swift
+ https://www.youtube.com/watch?v=hJ3v6MtLGnI&ab_channel=CodingInANutshell
+*/
 
 import Foundation
 
